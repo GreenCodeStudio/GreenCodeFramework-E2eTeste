53831b08cc044bc8af53026667d6ebc107255f1e
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2021-12-01 12:05:35 +0100

    start

---
+++ Test/Selenium/baseSeleniumTest.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+module.exports = class BaseSeleniumTest {
+    constructor(driver) {
+        this.driver = driver;
+    }
+
+    async takeScreenshot(file = 'default') {
+        let image = await this.driver.takeScreenshot()
+        const name = './screens/' + file + '-' + (new Date() * 1) + '.png';
+        fs.writeFile(name, image, 'base64', (err) => {
+            this.log(err);
+        });
+        this.log('Saving screnshot', name);
+    }
+
+    log(...args) {
+        console.log(new Date(), ...args);
+    }
+
+    asleep(x) {
+        return new Promise(resolve => setTimeout(resolve, x))
+    }
+}
---
+++ Test/Selenium/init.js
@@ -0,0 +1,66 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const util = require('util');
+const fs = require('fs');
+const net = require('net');
+const Xvfb = require("xvfb");
+const path = require("path");
+asleep(60000).then(() => process.exit(1));
+
+function asleep(x) {
+    return new Promise(resolve => setTimeout(resolve, x))
+}
+
+async function takeScreenshot(driver, file) {
+    let image = await driver.takeScreenshot()
+    fs.writeFile('./screens/' + file + '-' + (new Date() * 1) + '.png', image, 'base64', function (err) {
+        log(err);
+    });
+}
+
+function log(...args) {
+    console.log(new Date(), ...args);
+}
+
+function readDir(path) {
+    return new Promise((resolve, reject) => {
+        fs.readdir("./modules/", async (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files);
+            }
+        });
+    })
+}
+
+(async function run() {
+    var Xvfb = require('xvfb');
+    var xvfb = new Xvfb();
+    xvfb.startSync();
+
+    let driver = await new Builder().forBrowser('firefox').build();
+
+    const files = await readDir("./modules/");
+    let tests = [];
+    for (const file of files) {
+        const testPath = path.resolve(process.cwd(), `./modules/${file}/Test/Selenium/index.js`);
+        log(testPath);
+        if (fs.existsSync(testPath)) {
+            const obj = require(testPath);
+            log('obj:', obj);
+            tests.push(obj)
+        }
+    }
+    log('tests:', tests);
+    const testObjects = tests.map(x => new x(driver));
+    for (const test of testObjects) {
+        await test.prepareTest();
+    }
+    for (const test of testObjects) {
+        await test.mainTest();
+    }
+
+    driver.quit();
+    xvfb.stopSync();
+})();
