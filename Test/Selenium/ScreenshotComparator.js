b5f6fa2fe88d6c189fa624eff23e250544d570c6
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-06 14:27:19 +0200

    console log

--- Test/Selenium/ScreenshotComparator.js
+++ Test/Selenium/ScreenshotComparator.js
@@ -3,17 +3,18 @@ const fs = require("fs");
 
 module.exports = {
     async generateHtml() {
+        console.log('generatingHtml')
         let html = '<h1>Screnshot comparsion</h1>';
         for (const listComparsionElement of await this.listComparsion()) {
             html += `<div><img src="${listComparsionElement.diff}"><img src="${listComparsionElement.approved}"><img src="${listComparsionElement.current}"></div>`
         }
-       await  fs.writeFile('./screens/screnshotComparsion.html', html, '', (err) => {
-            this.log(err);
-        });
+       await  fs.writeFile('./screens/screnshotComparsion.html', html);
+        console.log('written ./screens/screnshotComparsion.html')
     }, async listComparsion() {
         let names = [...await fs.readdir('./screens'), ...await fs.readdir('./ApprovedScrenshots')];
         names = names.filter(x => x.endsWith('.png'));
         names = [...new Set(names)];
+        console.log({names})
         return await Promise.all(names.map(async name => {
             let approved = null;
             if (await fs.exists('./ApprovedScrenshots/' + name)) approved = 'data:image/png;base64,' + await fs.readFile('./ApprovedScrenshots/' + name, {encoding: 'base64'});
