d58dc3ee4df8deea3b5774e30181561db8622b2c
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-06 17:57:41 +0200

    .

--- Test/Selenium/ScreenshotComparator.js
+++ Test/Selenium/ScreenshotComparator.js
@@ -7,7 +7,9 @@ module.exports = {
         console.log('generatingHtml')
         let html = '<h1>Screnshot comparsion</h1>';
         diffNoticed=false;
-        for (const listComparsionElement of await this.listComparsion()) {
+        let list=await this.listComparsion();
+        list.sort((a,b)=>a.entropy-b.entropy);
+        for (const listComparsionElement of list) {
             html += `<div><img src="${listComparsionElement.diff}"><img src="${listComparsionElement.approved}"><img src="${listComparsionElement.current}"></div>`
             diffNoticed=diffNoticed||listComparsionElement.diffNoticed;
         }
@@ -27,20 +29,21 @@ module.exports = {
             if (fsClassic.existsSync('./screens/' + name)) current = 'data:image/png;base64,' + await fs.readFile('./screens/' + name, {encoding: 'base64'});
             let diff = null;
             let diffnoticed=false;
+            let entropy=0;
             if (approved && current) {
                 let diffImg = await sharp('./ApprovedScrenshots/' + name)
                     .composite([{
                         input: './screens/' + name, top: 0, left: 0, blend: 'difference'
                     },]).toBuffer();
                 let stats=await sharp(diffImg)                        .stats();
-                diffNoticed=stats.channels[0].max>50||stats.channels[1].max>50||stats.channels[2].max>50
+                entropy=stats.entropy
                 try {
                     console.log([...diffImg].slice(0, 100))
                     console.log(await sharp(diffImg)                        .stats())
                 }catch(ex){console.error(ex)}
                 diff = `data:image/png;base64,${diffImg.toString('base64')}`
             }
-            return {approved, current, diff, diffNoticed};
+            return {approved, current, diff, diffNoticed, entropy};
         }));
     }
 }
