a37470298ad29f315363037c73c2e4d4ad313026
Mateusz Krawczyk <mateusz.krawczyk@green-code.studio>
2022-07-06 13:57:09 +0200

    dif screnshots

--- Test/Selenium/ScreenshotComparator.js
+++ Test/Selenium/ScreenshotComparator.js
@@ -5,24 +5,29 @@ module.exports = {
     async generateHtml() {
         let html = '<h1>Screnshot comparsion</h1>';
         for (const listComparsionElement of await this.listComparsion()) {
-            html += `<div><img src="${listComparsionElement.approved}"><img src="${listComparsionElement.current}"></div>`
+            html += `<div><img src="${listComparsionElement.diff}"><img src="${listComparsionElement.approved}"><img src="${listComparsionElement.current}"></div>`
         }
         fs.writeFile('./screnshotComparsion.html', html, '', (err) => {
             this.log(err);
         });
-    },
-    async listComparsion() {
+    }, async listComparsion() {
         let names = [...await fs.readdir('./screens'), ...await fs.readdir('./ApprovedScrenshots')];
         names = names.filter(x => x.endsWith('.png'));
         names = [...new Set(names)];
         return await Promise.all(names.map(async name => {
             let approved = null;
-            if (await fs.exists('./ApprovedScrenshots/' + name))
-                approved = 'data:image/png;base64,' + await fs.readFile('./ApprovedScrenshots/' + name, {encoding: 'base64'});
+            if (await fs.exists('./ApprovedScrenshots/' + name)) approved = 'data:image/png;base64,' + await fs.readFile('./ApprovedScrenshots/' + name, {encoding: 'base64'});
             let current = null;
-            if (await fs.exists('./screens/' + name))
-                current = 'data:image/png;base64,' + await fs.readFile('./screens/' + name, {encoding: 'base64'});
-            return {approved, current};
+            if (await fs.exists('./screens/' + name)) current = 'data:image/png;base64,' + await fs.readFile('./screens/' + name, {encoding: 'base64'});
+            let diff = null;
+            if (approved && current) {
+                let diffImg = sharp('./ApprovedScrenshots/' + name)
+                    .composite([{
+                        input: './screens/' + name, top: 0, left: 0, blend: 'difference'
+                    },])
+                diff = `data:image/png;base64,${diffImg.toString('base64')}`
+            }
+            return {approved, current, diff};
         }));
     }
 }
