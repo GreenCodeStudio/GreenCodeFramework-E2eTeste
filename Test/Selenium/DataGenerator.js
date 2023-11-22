ee7dc8380889caf67599c7c2984a8e905c431696
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2023-11-22 17:56:09 +0100

    .

--- Test/Selenium/DataGenerator.js
+++ Test/Selenium/DataGenerator.js
@@ -1,13 +1,15 @@
-export const DataGenerator = {
-    generateRandomString (length=10) {
-        let text = '';
-        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        for (let i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
+module.exports= {
+    DataGenerator: {
+        generateRandomString(length = 10) {
+            let text = '';
+            const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            for (let i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return text;
+        },
+        generateRandomInt(min = 0, max = 100) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
         }
-        return text;
-    },
-    generateRandomInt (min=0, max=100) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
